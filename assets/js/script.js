303a475e2ad224c0371c81d9af75d91fafccc629
rdidjurgeit <raphael0rd@gmail.com>
2024-01-18 18:32:27 +0100

    Add question exemples with pictues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,27 @@
 // When DOM is loaded, run the game
 // Event listeners
 
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
+const questions =[
+    {
+        id:1
+        question:"54.When driving at 6 knots or more in a powered vessel, what is the minimum distance you must keep from moored vessels where possible?",
+        options:["A. 30 metres", "B. 10 metres", "C. 20 metres"],
+        correctAnswer:"A. 30 metres",
+        imageUrl:"/assets/images/54q.JPG"
+    },
+    {
+        id:2
+        question:"61.Looking at the cardinal mark pictured, what would be the safest side to drive your vessel?",
+        options:["A. East", "B. North","C. West"],
+        correctAnswer:"C. West",
+        imageUrl:"/assets/images/61q.JPG"
+    },
+    {
+        id:3
+        question:"102.Vessel A is using its sails and vessel B is using its engine. Who must give way?",
+        options:["A. Whichever vessel is travelling the slowest", "B. Vessel B  the powered vessel", "C. Vessel A  the sailing vessel"],
+        correctAnswer:"B. Vessel B  the powered vessel",
+        imageUrl:"/assets/images/102q.JPG"
+    },
+]
 
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
-            } else {
-                let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
-            }
-        });
-    }
-});
-
-function runGame(){
-
-}
-
-function checkAnswer(){
-
-}
-
-function calculateCorrectAnswer(){
-
-}
-
-function incrementScore(){
-
-}
-
-function incrementWrongAnswer(){
-
-}
-// function to display the questions
-function displayAdditionQuestion(){
-
-}
-
-function displaySubtractQuestion(){
-
-}
-function displayMultiplyQuestion(){
-
-}
-function displayDivisionQuestion(){
-
-}
