6f4d10174c073a6947820a7b3c1b2f5e72c2f5ca
rdidjurgeit <raphael0rd@gmail.com>
2024-01-19 21:47:41 +0100

    correct gramatic error in scriptr add btn-grid style

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,9 @@
 const startButton = document.getElementById('start-btn')
 const startContainer = document.getElementById('start-container')
 const questionContainerElemnt = document.getElementById('question-container')
+const controlContainerElemnt = document.getElementById('controls')
 const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('andswer-buttons')
+const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions,  currentQuestionIndex
 
 
@@ -14,9 +15,10 @@ startButton.addEventListener('click', startGame)
 function startGame(){
     startButton.classList.add("hide")
     startContainer.classList.add("hide")
+    questionContainerElemnt.classList.remove('hide')
+    controlContainerElemnt.classList.remove('hide')  
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
-    questionContainerElement.classList.remove('hide') 
     setNextQuestion()
 
 }
@@ -24,9 +26,21 @@ function startGame(){
 function setNextQuestion(){
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
+
 function showQuestion(question){
- questionElement.innerText = question.question
+    questionElement.innerText = question.question
+    question.answer.forEach(answer => {
+        const button =document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+    if (answer.correct) {
+        button.dataset.correct = answer.correct
+    }
+    button.addEventListenner('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+})
 }
+
 
 function selectAnswer(){
 
@@ -34,32 +48,28 @@ function selectAnswer(){
 
 const questions =[
  {
-question:"54.When driving at 6 knots or more in a powered vessel, what is the minimum distance you must keep from moored vessels where possible?",
-    options:[
-    {text: "A. 30 metres", correct: true},
-    {text: "B. 10 metres", correct: false},
-    {text: "C. 20 metres", correct: false}
+question:'54.When driving at 6 knots or more in a powered vessel, what is the minimum distance you must keep from moored vessels where possible?',
+answer:[
+    {text: 'A. 30 metres', correct: true},
+    {text: 'B. 10 metres', correct: false},
+    {text: 'C. 20 metres', correct: false}
     ],
-imageUrl:"/assets/images/54q.JPG"
 },
 {
-question:"61.Looking at the cardinal mark pictured, what would be the safest side to drive your vessel?",
-options:[
-    {text: "A. East", correct: false},
-    {text: "B. North", correct: false},
-    {text: "C. West", correct: true}
+question:'61.Looking at the cardinal mark pictured, what would be the safest side to drive your vessel?',
+answer:[
+    {text: 'A. East', correct: false},
+    {text: 'B. North', correct: false},
+    {text: 'C. West', correct: true}
 ],
-imageUrl:"/assets/images/61q.JPG"
 },
  {
-question:"102.Vessel A is using its sails and vessel B is using its engine. Who must give way?",
-options:[
-    {text: "A. Whichever vessel is travelling the slowest", correct: false}, 
-    {text: "B. Vessel B  the powered vessel", correct: true},
-    {text: "C. Vessel A  the sailing vessel", correct: false}
-],
-correctAnswer:"B. Vessel B  the powered vessel",
-imageUrl:"/assets/images/102q.JPG"
+question:'102.Vessel A is using its sails and vessel B is using its engine. Who must give way?',
+answer:[
+    {text: 'A. Whichever vessel is travelling the slowest', correct: false}, 
+    {text: 'B. Vessel B  the powered vessel', correct: true},
+    {text: 'C. Vessel A  the sailing vessel', correct: false}
+]
  },
 ]
 
