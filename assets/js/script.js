c2395eb59438b60c3a0e1d982c3285353b4e1e51
rdidjurgeit <raphael0rd@gmail.com>
2024-01-19 14:14:40 +0100

    change the way question are desplay

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ function startGame(){
     console.log(startGame)
     startButton.classList.add("hide")
     startContainer.classList.add("hide")
+    setNextQuestion()
 
 }
 
@@ -20,27 +21,34 @@ function selectAnswer(){
 
 }
 
-// const questions =[
-//     {
-//         id:1
-//         question:"54.When driving at 6 knots or more in a powered vessel, what is the minimum distance you must keep from moored vessels where possible?",
-//         options:["A. 30 metres", "B. 10 metres", "C. 20 metres"],
-//         correctAnswer:"A. 30 metres",
-//         imageUrl:"/assets/images/54q.JPG"
-//     },
-//     {
-//         id:2
-//         question:"61.Looking at the cardinal mark pictured, what would be the safest side to drive your vessel?",
-//         options:["A. East", "B. North","C. West"],
-//         correctAnswer:"C. West",
-//         imageUrl:"/assets/images/61q.JPG"
-//     },
-//     {
-//         id:3
-//         question:"102.Vessel A is using its sails and vessel B is using its engine. Who must give way?",
-//         options:["A. Whichever vessel is travelling the slowest", "B. Vessel B  the powered vessel", "C. Vessel A  the sailing vessel"],
-//         correctAnswer:"B. Vessel B  the powered vessel",
-//         imageUrl:"/assets/images/102q.JPG"
-//     },
-// ]
+const questions =[
+ {
+question:"54.When driving at 6 knots or more in a powered vessel, what is the minimum distance you must keep from moored vessels where possible?",
+    options:[
+    {text: "A. 30 metres", correct: true},
+    {text: "B. 10 metres", correct: false},
+    {text: "C. 20 metres", correct: false}
+    ],
+imageUrl:"/assets/images/54q.JPG"
+},
+{
+question:"61.Looking at the cardinal mark pictured, what would be the safest side to drive your vessel?",
+options:[
+    {text: "A. East", correct: false},
+    {text: "B. North", correct: false},
+    {text: "C. West", correct: true}
+],
+imageUrl:"/assets/images/61q.JPG"
+},
+ {
+question:"102.Vessel A is using its sails and vessel B is using its engine. Who must give way?",
+options:[
+    {text: "A. Whichever vessel is travelling the slowest", correct: false}, 
+    {text: "B. Vessel B  the powered vessel", correct: true},
+    {text: "C. Vessel A  the sailing vessel", correct: false}
+],
+correctAnswer:"B. Vessel B  the powered vessel",
+imageUrl:"/assets/images/102q.JPG"
+ },
+]
 
