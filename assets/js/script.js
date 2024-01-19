3f4977ebcfb4da8a69e6cada92787c00750be723
rdidjurgeit <raphael0rd@gmail.com>
2024-01-19 13:47:34 +0100

    add a bet html and some gramatic correction add  interaction in start-btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 // When DOM is loaded, run the game
 // Event listeners
 
-const startButton = document.getElementById(start-btn)
+const startButton = document.getElementById('start-btn')
 
-startButton.addEventListener('cluck', startGame)
+startButton.addEventListener('click', startGame)
 
-function startGame (){
+function startGame(){
+    console.log(startGame)
+    startButton.classList.add("hide")
 
 }
 
@@ -13,31 +15,31 @@ function setNextQuestion(){
 
 }
 
-function select Answer(){
-    
+function selectAnswer(){
+
 }
 
-const questions =[
-    {
-        id:1
-        question:"54.When driving at 6 knots or more in a powered vessel, what is the minimum distance you must keep from moored vessels where possible?",
-        options:["A. 30 metres", "B. 10 metres", "C. 20 metres"],
-        correctAnswer:"A. 30 metres",
-        imageUrl:"/assets/images/54q.JPG"
-    },
-    {
-        id:2
-        question:"61.Looking at the cardinal mark pictured, what would be the safest side to drive your vessel?",
-        options:["A. East", "B. North","C. West"],
-        correctAnswer:"C. West",
-        imageUrl:"/assets/images/61q.JPG"
-    },
-    {
-        id:3
-        question:"102.Vessel A is using its sails and vessel B is using its engine. Who must give way?",
-        options:["A. Whichever vessel is travelling the slowest", "B. Vessel B  the powered vessel", "C. Vessel A  the sailing vessel"],
-        correctAnswer:"B. Vessel B  the powered vessel",
-        imageUrl:"/assets/images/102q.JPG"
-    },
-]
+// const questions =[
+//     {
+//         id:1
+//         question:"54.When driving at 6 knots or more in a powered vessel, what is the minimum distance you must keep from moored vessels where possible?",
+//         options:["A. 30 metres", "B. 10 metres", "C. 20 metres"],
+//         correctAnswer:"A. 30 metres",
+//         imageUrl:"/assets/images/54q.JPG"
+//     },
+//     {
+//         id:2
+//         question:"61.Looking at the cardinal mark pictured, what would be the safest side to drive your vessel?",
+//         options:["A. East", "B. North","C. West"],
+//         correctAnswer:"C. West",
+//         imageUrl:"/assets/images/61q.JPG"
+//     },
+//     {
+//         id:3
+//         question:"102.Vessel A is using its sails and vessel B is using its engine. Who must give way?",
+//         options:["A. Whichever vessel is travelling the slowest", "B. Vessel B  the powered vessel", "C. Vessel A  the sailing vessel"],
+//         correctAnswer:"B. Vessel B  the powered vessel",
+//         imageUrl:"/assets/images/102q.JPG"
+//     },
+// ]
 
